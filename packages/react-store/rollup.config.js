0b1508cdaee505258c8567432139641948da0741
Matheus Albino <matheusalbino@users.noreply.github.com>
2022-07-28 19:42:37 -0300

    chore(react-store): update project configuration

--- packages/react-store/rollup.config.js
+++ packages/react-store/rollup.config.js
@@ -1,4 +1,3 @@
-import replace from '@rollup/plugin-replace';
 import { defineConfig } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
@@ -6,21 +5,22 @@ import typescript from 'rollup-plugin-typescript2';
 
 export default defineConfig({
   input: ['src/index.ts'],
-  output: {
-    sourcemap: true,
-    dir: 'lib',
-    format: 'esm',
-  },
+  output: [
+    {
+      sourcemap: true,
+      dir: 'lib/esm',
+      format: 'esm',
+    },
+    {
+      sourcemap: true,
+      dir: 'lib/cjs',
+      format: 'cjs',
+    },
+  ],
   external: ['react'],
   plugins: [
-    replace({
-      preventAssignment: true,
-      values: {
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
     commonjs(),
     nodeResolve({ preferBuiltins: true }),
-    typescript(),
+    typescript({ useTsconfigDeclarationDir: true }),
   ],
 });
