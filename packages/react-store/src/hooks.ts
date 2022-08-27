7466653ffaf9228ed4416c805680d1812789cd85
Equal <6087948+is-equal@users.noreply.github.com>
2022-08-26 21:20:22 -0300

    refactor(react-store): change hooks implementation to use React18 API

--- packages/react-store/src/hooks.ts
+++ packages/react-store/src/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useSyncExternalStore } from 'react';
 import type { AnyStore, StoreMutations, StoreData } from '@equal/store';
 
 export function useStore<Store extends AnyStore>(
@@ -14,28 +14,12 @@ export function useStoreSubscriber<Store extends AnyStore>(store: Store): void {
   useStoreValue(store);
 }
 
-export function useStoreMutations<Store extends AnyStore, Mutations extends StoreMutations<Store>>(
-  store: Store
-): Mutations {
-  return store as unknown as Mutations;
+export function useStoreMutations<Store extends AnyStore>(store: Store): StoreMutations<Store> {
+  return store as unknown as StoreMutations<Store>;
 }
 
 export function useStoreValue<Store extends AnyStore>(store: Store): StoreData<Store> {
-  const [, forceRender] = useState(true);
-  const data = useRef<StoreData<Store>>(store.read());
-
-  useEffect(() => {
-    return store.subscribe(function storeListener(value) {
-      if (Object.is(data.current, value)) {
-        return;
-      }
-
-      data.current = value;
-      forceRender((next) => !next);
-    });
-  }, [store]);
-
-  return data.current;
+  return useSyncExternalStore(store.subscribe, store.read);
 }
 
 type StoreSelector<Data, R = any> = (value: Data | undefined) => R;
@@ -44,21 +28,5 @@ export function useStoreSelector<
   Store extends AnyStore,
   Selector extends StoreSelector<StoreData<Store>>
 >(store: Store, selector: Selector): ReturnType<Selector> {
-  const [, forceRender] = useState(true);
-  const data = useRef<ReturnType<Selector>>(selector(store.read()));
-
-  useEffect(() => {
-    return store.subscribe(function storeListener(value) {
-      const nextValue = selector(value);
-
-      if (Object.is(data.current, nextValue)) {
-        return;
-      }
-
-      data.current = nextValue;
-      forceRender((next) => !next);
-    });
-  }, [store, selector]);
-
-  return data.current;
+  return useSyncExternalStore(store.subscribe, () => selector(store.read()));
 }
